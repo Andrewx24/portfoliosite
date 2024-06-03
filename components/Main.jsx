93a28df63126ba54c282bda36cd956cf19f277e6
Andrew Aliaj <andyaliaj@gmail.com>
2024-06-03 10:50:03 -0400

    more fixes

--- components/Main.jsx
+++ components/Main.jsx
@@ -12,41 +12,46 @@ const Main = () => {
   const [collapse, setCollapse] = useState(true);
 
   return (
-    <div  className="w-full text-center container">
+    <div className="w-full text-center container mx-auto">
       <Head>
         <title>Andrew Aliaj - Home</title>
       </Head>
       <main
-        className={
-          collapse
-            ? "max-w-[1240px] mx-auto p-2 flex justify-center items-center"
-            : "mt-60"
-        }
+        className={`max-w-[1240px] mx-auto p-2 flex flex-col justify-center items-center ${
+          collapse ? "min-h-screen" : "mt-20"
+        }`}
       >
         <div>
-          <h1 className="animate__animated animate__zoomInDown py-4 text-gray-700 dark:text-white">
-           Full Stack Developer
+          <h1 className="animate__animated animate__zoomInDown py-4 text-gray-700 dark:text-white text-3xl sm:text-4xl lg:text-5xl">
+            Full Stack Developer
           </h1>
-   
-          <p className="py-4 text-gray-600   max-w-fit m-auto dark:text-white">
-           I am a  
+
+          <p className="py-4 text-gray-600 max-w-fit mx-auto dark:text-white text-lg sm:text-xl">
+            I am a
             <span className="text-[#21568a] font-bold dark:text-white">
               <strong> Software Engineer</strong>
             </span>{" "}
-            and {" "}
+            and{" "}
             <span className="text-[#21568a] font-bold dark:text-white">
               <strong>Full Stack Developer</strong>
             </span>{" "}
-            based in the  New York City area,  specializing in <span className="text-[#21568a] font-bold dark:text-white"> <strong>Javascript,React,Next.js,Node.js,Typescript,Python,SQL, and AWS.</strong> </span> Details are listed below. 
+            based in the New York City area, specializing in{" "}
+            <span className="text-[#21568a] font-bold dark:text-white">
+              <strong>
+                JavaScript, React, Next.js, Node.js, TypeScript, Python, SQL,
+                and AWS.
+              </strong>
+            </span>{" "}
+            Details are listed below.
           </p>
-          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+          <div className="flex items-center justify-between max-w-[300px] sm:max-w-[330px] mx-auto py-4">
             <a
               href="https://www.linkedin.com/in/andrewaliaj/"
               target="_blank"
               rel="noreferrer"
             >
               <Tooltip content={"LinkedIn"} rounded color="primary">
-                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 dark:bg-white dark:shadow-none">
+                <div className="rounded-full shadow-lg shadow-gray-400 p-4 sm:p-6 cursor-pointer hover:scale-110 ease-in duration-300 dark:bg-white dark:shadow-none">
                   <FaLinkedinIn />
                 </div>
               </Tooltip>
@@ -57,24 +62,23 @@ const Main = () => {
               rel="noreferrer"
             >
               <Tooltip content={"GitHub"} rounded color="primary">
-                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 dark:bg-white dark:shadow-none">
+                <div className="rounded-full shadow-lg shadow-gray-400 p-4 sm:p-6 cursor-pointer hover:scale-110 ease-in duration-300 dark:bg-white dark:shadow-none">
                   <FaGithub />
                 </div>
               </Tooltip>
             </a>
             <Tooltip content={"Email me below"} rounded color="primary">
               <Link href="/#contact">
-                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300 dark:bg-white dark:shadow-none">
+                <div className="rounded-full shadow-lg shadow-gray-400 p-4 sm:p-6 cursor-pointer hover:scale-110 ease-in duration-300 dark:bg-white dark:shadow-none">
                   <AiOutlineMail />
                 </div>
               </Link>
             </Tooltip>
-    
           </div>
         </div>
       </main>
     </div>
   );
-}
+};
 
 export default Main;
